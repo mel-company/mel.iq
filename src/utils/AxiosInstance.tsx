49bba8fd679256b4048a84c18e457712b6196648
bnjamel <malfaris4771@gmail.com>
2026-02-21 10:35:19 +0300

    Restore server proxy configuration in Vite and revert Axios instance base URL handling to use a relative path. This change re-enables the proxy for API requests to localhost:3000 and simplifies the base URL setup for better integration with the Vite environment.

--- src/utils/AxiosInstance.tsx
+++ src/utils/AxiosInstance.tsx
@@ -16,9 +16,9 @@ import axios from "axios";
 const baseURL = "/api/v1";
 
 const axiosInstance = axios.create({
-  // baseURL,
-  baseURL:
-    import.meta.env.VITE_API_BASE_URL || "http://192.168.10.164:3000/api/v1",
+  baseURL,
+  // baseURL:
+  //   import.meta.env.VITE_API_BASE_URL || "http://192.168.10.164:3000/api/v1",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
